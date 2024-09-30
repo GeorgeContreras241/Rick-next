c6a9e16a53c4926413a7c6f04275b093ee600c6d
GeorgeContreras241 <maicolcontreras241@gmail.com>
2024-09-30 14:41:10 -0500

    bug vercel

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,14 +6,11 @@ export default function Home() {
       <div className={home.container__home}>
         <h3>Rick and morty one of the best cartoons</h3>
         <p>
-          After being missing for almost 20 years, Rick Sanchez unexpectedly arrives at his daughter Beth's doorstep and
+          After being missing for almost 20 years, Rick Sanchez unexpectedly arrives at his daughter Beth is doorstep and
           moves in with her and her family, using the garage as his personal laboratory.
         </p>
         <div className={home.container__buttons}>
           <button className={home.button__home} >Home</button>
-          <a href=""></a>
-          <a href=""></a>
-          <a href=""></a>
         </div>
       </div>
     </>
